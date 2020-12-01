f6bb55f71a954f6bd3f949c62729f575179b4358
Nathan Jen <njen@andrew.cmu.edu>
2020-12-01 11:55:55 -0500

    added scroll to top

--- src/index.js
+++ src/index.js
@@ -4,9 +4,11 @@ import './index.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter as Router } from "react-router-dom";
+import ScrollToTop from './components/ScrollToTop/ScrollToTop'
 
 ReactDOM.render(
   <Router>
+    <ScrollToTop />
     <App />
   </Router>,
   document.getElementById('root')
