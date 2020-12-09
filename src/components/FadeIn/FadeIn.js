2384092fc927edd94bf089a167aae0f803decc80
Nathan Jen <njen@andrew.cmu.edu>
2020-12-08 20:43:32 -0500

    fade ins only happening on first viewing

--- src/components/FadeIn/FadeIn.js
+++ src/components/FadeIn/FadeIn.js
@@ -4,19 +4,21 @@ import styled from "styled-components"
 // code inspiration from https://www.selbekk.io/blog/2019/08/how-to-fade-in-content-as-it-scrolls-into-view/
 export default function FadeIn(props) {
   const [isVisible, setVisible] = useState(false)
+  const [hasViewed, setHasViewed] = useState(false)
   const domRef = useRef()
 
   useEffect(() => {
     const observer = new IntersectionObserver(entries => {
       entries.forEach(entry => {
         setVisible(entry.isIntersecting)
+        entry.isIntersecting && setHasViewed(true)
       })
     })
     observer.observe(domRef.current)
   }, []);
-
+  
   return (
-    <Fade visible={isVisible} ref={domRef}>
+    <Fade visible={isVisible || hasViewed} ref={domRef}>
       {props.children}
     </Fade>
   )
@@ -24,7 +26,7 @@ export default function FadeIn(props) {
 
 const Fade = styled.div`
   opacity: 0;
-  transform: translateY(20vh);
+  // transform: translateY(10vh);
   visibility: hidden;
   transition: opacity 0.6s ease-out, transform 1.2s ease-out;
   will-change: opacity, visibility;
